36f801a4f974268a4a17a00673f9f57bcb030ac4
Shakil R. <tuku.co@hotmail.com>
2026-08-29 06:50:46 +0600

    Fix URL in GOOGLE_SCRIPT_URL constant

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const GOOGLE_SCRIPT_URL =
 */
 
 const GOOGLE_SCRIPT_URL =
-    "Phttps://script.google.com/macros/s/AKfycbw7_durhcn8Hy7BHhZwXTy3Alvm-1C11ywErq3rt5C-I323l2pa3lsf4x8Up6UBC29o/exec";
+    "https://script.google.com/macros/s/AKfycbw7_durhcn8Hy7BHhZwXTy3Alvm-1C11ywErq3rt5C-I323l2pa3lsf4x8Up6UBC29o/exec";
 
 
 
